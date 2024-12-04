53d82beb3b36496c4b2fa8176c975e68ef693095
nobleycs <1459175045@qq.com>
2024-12-04 10:07:40 +0800

    aaa

---
+++ SpringLearn/IoC/src/main/java/com/atYCS/CommonComponent.java
@@ -0,0 +1,7 @@
+package com.atYCS;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonComponent {
+}
---
+++ SpringLearn/IoC/src/main/java/com/atYCS/XxxDao.java
@@ -0,0 +1,7 @@
+package com.atYCS;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class XxxDao {
+}
